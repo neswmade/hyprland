5eeaf11b273bb8c19589eeefeb5fadf91f200cb4
liammmcauliffe <lmmcauliffe1223@gmail.com>
2026-06-25 22:43:08 -0400

    refactor(config/gestures): WIP add gestures.lua

---
+++ config/gestures.lua
@@ -0,0 +1,37 @@
+local vars = require("variables")
+
+-- swipe tuning
+hl.config({
+    gestures = {
+        workspace_swipe_distance = 700,
+        workspace_swipe_cancel_ratio = 0.15,
+        workspace_swipe_min_speed_to_force = 5,
+        workspace_swipe_direction_lock = true,
+        workspace_swipe_direction_lock_threshold = 10,
+        workspace_swipe_create_new = true,
+        workspace_swipe_use_r = true,
+        workspace_swipe_forever = true,
+    },
+})
+
+hl.gestures({
+    fingers = vars.workspaceSwipeFingers,
+    direction = "horizontal",
+    action = "workspace"
+})
+
+hl.gestures({
+    fingers = vars.gestureFingers,
+    direction = "up",
+    action = "special",
+    arg = "special"
+})
+
+hl.gestures({
+    fingers = vars.gestureFingersMore,
+    direction = "down",
+    action = function()
+        hl.exec_cmd(vars.suspendCommand)
+    end,
+})
+
