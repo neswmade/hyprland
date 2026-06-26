7b286f173297fc31580491617c22f58fce11b47e
liammmcauliffe <lmmcauliffe1223@gmail.com>
2026-06-25 22:52:58 -0400

    refactor(config/decorations): WIP add decorations.lua

---
+++ config/decorations.lua
@@ -0,0 +1,19 @@
+local vars = require("variables")
+
+hl.config({
+    decoration = {
+        rounding = vars.windowRounding,
+
+        blur = {
+            enabed = vars.blurEnabled,
+            xray = vars.blurXray,
+            special = vars.blurSpecialWs,
+            ignore_opacity = true,
+            new_optimizations = true,
+            popups = vars.blurPopups,
+            input_methods = vars.blurInputMethods,
+            size = vars.blurSize,
+            passes = vars.blurPasses,
+        },
+    },
+})
